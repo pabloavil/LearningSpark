86f4ea547d161b3b86e27d04afb1266e5b78a446
Pablo De Ávila <Pablo De vila@DESKTOP-SF1F4AE>
2020-09-07 08:49:36 +0200

    Descomentar trozos

--- src/capitulo2/MM.scala
+++ src/capitulo2/MM.scala
@@ -8,60 +8,60 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.streaming.Time
 
 object MM {
-  
-  case class RegistroMM(State:String,Color:String,Count:BigInt){
-    
-  }
-  
-  
-	def main(args: Array[String]) {
-		Logger.getLogger("org").setLevel(Level.ERROR)
-		// Create a SparkContext using every core of the local machine
-	
 
-		val spark = SparkSession.builder().getOrCreate()
-   import spark.implicits._
+case class RegistroMM(State:String,Color:String,Count:BigInt){
+
+}
 
 
-		val mM = spark.read.format("csv")
-		.option("header",true)
-		.option("inferSchema", "true")
-		.load("mnm_dataset.csv")
-		
-		val mMDS =mM.as[RegistroMM]
-
-		mM.createOrReplaceTempView("MM")             
-
-		//ver tiempos
-//		mM.show()
-		//spark.sql("Select * From MM").show()
-
-//		val countMM =mM.groupBy("State", "Color").agg(count("count").alias("Count")).orderBy("Color")
-//		countMM.show()
-//		
-//		val countMMDS = mMDS.groupBy("State", "Color").agg(count("count").alias("Count")).orderBy("Color").as[RegistroMM]
-//		countMMDS.show()
-//		
-//		spark.sql("Select State, Color , count(count) as Total from MM Group BY State ,Color Order By Color ").show()
+def main(args: Array[String]) {
+	Logger.getLogger("org").setLevel(Level.ERROR)
+	// Create a SparkContext using every core of the local machine
 
 
-//		val caTxCountMnNDF = mM.select("*") //Si seleccionas todo creo que no es necesaria la clausula
-//		.where(col("State") === "CA" ||col("State") === "TX" ) //Y asi todos los estrados que quieras
-//		.groupBy("State", "Color")
-//		.agg(count("Count")
-//				.alias("Total"))
-//		.orderBy(desc("Total"))
-//		 
-//	  caTxCountMnNDF.show(10)	
-//		
-//		spark.sql("Select State, Color , count(count) as Total From MM Where State IN ('TX','CA','CO') Group BY State ,Color Order By Color ").show()
-//		
-		//Vamos a calcular  Min, Max, Avg y Count en una sola sentencia
-		
-		mM.groupBy("State", "Color").agg(min($"Count").alias("Min"), max("count").alias("Max"),round(avg("count"),2).alias("Media"),count("count").alias("Contador")).show()
-		mMDS.groupBy("State", "Color").agg(min($"Count").alias("Min"), max("count").alias("Max"),round(avg("count"),2).alias("Media"),count("count").alias("Contador")).show()
+	val spark = SparkSession.builder().getOrCreate()
+	import spark.implicits._
 
 
-	}
+	val mM = spark.read.format("csv")
+	.option("header",true)
+	.option("inferSchema", "true")
+	.load("mnm_dataset.csv")
+
+	val mMDS =mM.as[RegistroMM]
+
+	mM.createOrReplaceTempView("MM")             
+
+	//ver tiempos
+	mM.show()
+	spark.sql("Select * From MM").show()
+
+	val countMM =mM.groupBy("State", "Color").agg(count("count").alias("Count")).orderBy("Color")
+	countMM.show()
+
+	val countMMDS = mMDS.groupBy("State", "Color").agg(count("count").alias("Count")).orderBy("Color").as[RegistroMM]
+			countMMDS.show()
+
+	spark.sql("Select State, Color , count(count) as Total from MM Group BY State ,Color Order By Color ").show()
+
+
+	val caTxCountMnNDF = mM.select("*") //Si seleccionas todo creo que no es necesaria la clausula
+	.where(col("State") === "CA" ||col("State") === "TX" ) //Y asi todos los estrados que quieras
+	.groupBy("State", "Color")
+	.agg(count("Count")
+			.alias("Total"))
+	.orderBy(desc("Total"))
+
+	caTxCountMnNDF.show(10)	
+
+	spark.sql("Select State, Color , count(count) as Total From MM Where State IN ('TX','CA','CO') Group BY State ,Color Order By Color ").show()
+	//		
+	//Vamos a calcular  Min, Max, Avg y Count en una sola sentencia
+
+	mM.groupBy("State", "Color").agg(min($"Count").alias("Min"), max("count").alias("Max"),round(avg("count"),2).alias("Media"),count("count").alias("Contador")).show()
+	mMDS.groupBy("State", "Color").agg(min($"Count").alias("Min"), max("count").alias("Max"),round(avg("count"),2).alias("Media"),count("count").alias("Contador")).show()
+
+
+}
 
 }
--- src/capitulo2/quijote.scala
+++ src/capitulo2/quijote.scala
@@ -9,32 +9,32 @@ import org.apache.spark.sql.SparkSession
 
 
 object quijote {
-   def main(args: Array[String]) {
-     Logger.getLogger("org").setLevel(Level.ERROR)
-        
-    // Create a SparkContext using every core of the local machine
-    
-    val spark = SparkSession.builder().appName("quijote").getOrCreate()
-    
-   
-    
-    val quijote = spark.read.text("el_quijote.txt")
-    
-   
-    quijote.show(false)       //muestra 20 filas sin truncar 
-//    quijote.show(true)        //20 filas true trunca a 20 caracteres y alinea a derecha (por defecto)
-//    quijote.show(10)          //num filas
-//    quijote.show(10,true)     //num filas ,true trunca a 20 caracteres y alinea a derecha
-//    quijote.show(10,10)       //numero filas, truncate strings (0 no truca)
+	def main(args: Array[String]) {
+		Logger.getLogger("org").setLevel(Level.ERROR)
 
-//    quijote.show(10,10,true)  //numero filas, truncate strings (0 no truca) muestra cada atributo debajo del anterior
-    
-   println( quijote.count())
-    
-   println( quijote.head()) //Coje las primeras x filas , por defecto 1 return Row
-    quijote.head(10).foreach(println)  //Return Array[Row]
-    quijote.first() //Es un alias de .head() pero no admite parametros
-    quijote.take(12) //Manda al drivcer las x primeras filas CUIDADOOOO Puede dar error de memoria
-    
-   }
+		// Create a SparkContext using every core of the local machine
+
+		val spark = SparkSession.builder().appName("quijote").getOrCreate()
+
+
+
+		val quijote = spark.read.text("el_quijote.txt")
+
+
+		quijote.show(false)       //muestra 20 filas sin truncar 
+		quijote.show(true)        //20 filas true trunca a 20 caracteres y alinea a derecha (por defecto)
+		quijote.show(10)          //num filas
+		quijote.show(10,true)     //num filas ,true trunca a 20 caracteres y alinea a derecha
+		quijote.show(10,10)       //numero filas, truncate strings (0 no truca)
+
+		quijote.show(10,10,true)  //numero filas, truncate strings (0 no truca) muestra cada atributo debajo del anterior
+
+		println( quijote.count())
+
+		println( quijote.head()) //Coje las primeras x filas , por defecto 1 return Row
+		quijote.head(10).foreach(println)  //Return Array[Row]
+		quijote.first() //Es un alias de .head() pero no admite parametros
+		quijote.take(12) //Manda al drivcer las x primeras filas CUIDADOOOO Puede dar error de memoria
+
+	}
 }
